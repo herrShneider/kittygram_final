58a2047722c88f80e7bbd557c0198a0f12123030
Владимир Ласовский <herrShneider@gmail.com>
2024-04-10 20:31:00 +0200

    Add constatnts from settings.py to .env

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,18 +1,14 @@
 import os
 from pathlib import Path
 
-# from dotenv import load_dotenv
-#
 BASE_DIR = Path(__file__).resolve().parent.parent
-# print('BASE_DIR=          ', BASE_DIR.parent)
-#
-# load_dotenv(BASE_DIR.parent)
+
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
 
 DEBUG = False
 
 # ALLOWED_HOSTS = ['51.250.18.73', '127.0.0.1', 'localhost', 'kittygram-las.hopto.org']
-ALLOWED_HOSTS = ['51.250.18.73', '127.0.0.1', 'localhost', 'kittygram-las.hopto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -96,7 +92,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
-# STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
