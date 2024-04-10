4d5ace98d96c5a4c77750e66440051b2d2ae6ce3
Владимир Ласовский <herrShneider@gmail.com>
2024-04-10 21:34:57 +0200

    edit settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.18.73', '127.0.0.1', 'localhost', 'kittygram-las.hopto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
